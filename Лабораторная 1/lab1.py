a5eec5cc1e31c6a649530faf1c2e9f549ff30321
Amir_Gapurov <gapurovamir03@gmail.com>
2026-09-23 14:14:52 +0500

    Create lab1.py

---
+++ Лабораторная 1/lab1.py
@@ -0,0 +1,9 @@
+kilometers = float(input("Введите количество километров: "))
+
+meters = kilometers * 1000
+centimeters = kilometers * 100000
+millimeters = kilometers * 1000000
+
+print("Метры:", meters)
+print("Сантиметры:", centimeters)
+print("Миллиметры:", millimeters)
